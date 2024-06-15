5bdfccea50fd39f46a407f280c2fb38ea1f7b1aa
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-15 17:05:37 -0500

    rewrote suit.java to include the four different elements of the game (fire, earth, air, water)

--- src/edu/westga/cs6910/elementalclash/model/Suit.java
+++ src/edu/westga/cs6910/elementalclash/model/Suit.java
@@ -7,7 +7,19 @@ package edu.westga.cs6910.elementalclash.model;
  * @version Summer 2024
  */
 public enum Suit {
+    FIRE("Fire"),
+    EARTH("Earth"),
+    AIR("Air"),
+    WATER("Water");
 
-	SPADES
+    private final String displayName;
 
+    Suit(String displayName) {
+        this.displayName = displayName;
+    }
+
+    @Override
+    public String toString() {
+        return this.displayName;
+    }
 }
