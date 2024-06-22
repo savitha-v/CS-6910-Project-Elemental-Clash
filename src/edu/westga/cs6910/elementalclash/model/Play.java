6c3708ff0a545a005902be4c08acbcad368c8556
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-22 14:54:36 -0500

    updated play interface so that both computer and human player's hands can be displayed

--- src/edu/westga/cs6910/elementalclash/model/Play.java
+++ src/edu/westga/cs6910/elementalclash/model/Play.java
@@ -1,19 +1,11 @@
 package edu.westga.cs6910.elementalclash.model;
 
-/**
- * The Play interface. 
- * The interface Play defines the method signatures for the behaviors that every player object must implement.
- * 
- * @version 06/22/2024
- * @author Savitha Venkatesh
- */
+import java.util.List;
+
 public interface Play {
-	
     String getName();
-    
     int getLifePoints();
-    
     Card drawCard();
-    
     void reduceLifePoints(int points);
+    List<Card> getHand();
 }
