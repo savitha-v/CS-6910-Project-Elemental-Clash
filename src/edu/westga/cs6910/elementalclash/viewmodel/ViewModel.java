fb5ab29715a48bd54778c77ab641711afe108ff4
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-22 14:52:38 -0500

    updated viewmodel to reflect game logic

--- src/edu/westga/cs6910/elementalclash/viewmodel/ViewModel.java
+++ src/edu/westga/cs6910/elementalclash/viewmodel/ViewModel.java
@@ -1,25 +1,47 @@
 package edu.westga.cs6910.elementalclash.viewmodel;
 
+import edu.westga.cs6910.elementalclash.model.Game;
+import edu.westga.cs6910.elementalclash.model.HumanPlayer;
+import edu.westga.cs6910.elementalclash.model.ComputerPlayer;
+import edu.westga.cs6910.elementalclash.model.Deck;
+import edu.westga.cs6910.elementalclash.model.Play;
 import edu.westga.cs6910.elementalclash.model.Card;
-import edu.westga.cs6910.elementalclash.model.Rank;
-import edu.westga.cs6910.elementalclash.model.Suit;
+import java.util.List;
 
-/**
- * The Class ViewModel.
- * 
- * @version 06/16/2024
- * @author Savitha Venkatesh
- */
 public class ViewModel {
+    private Game game;
+    
+    public ViewModel() {
+        Deck deck = new Deck();
+        this.game = new Game(new HumanPlayer("Human", deck), new ComputerPlayer("Computer", deck), deck);
+    }
 
-    /**
-     * Deals a specific card.
-     * 
-     * @precondition none
-     * @postcondition none
-     * @return the dealt card
-     */
-    public Card dealCard() {
-        return new Card(Rank.COMMON_1, Suit.AIR);
+    public void startGame() {
+        this.game.start();
+    }
+
+    public void playRound() {
+        this.game.playRound();
+    }
+
+    public boolean isGameOver() {
+        return this.game.isGameOver();
+    }
+
+    public String getWinner() {
+        Play winner = this.game.getWinner();
+        if (winner != null) {
+            return winner.getName();
+        } else {
+            return "No winner yet";
+        }
+    }
+
+    public List<Card> getHumanHand() {
+        return this.game.getHumanPlayer().getHand();
+    }
+
+    public List<Card> getComputerHand() {
+        return this.game.getComputerPlayer().getHand();
     }
 }
