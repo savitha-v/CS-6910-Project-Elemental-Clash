a27419a9e9e65d79db375a62a2a1e4f5fca94d16
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-22 18:10:49 -0500

    included javadoc comments

--- src/edu/westga/cs6910/elementalclash/model/AbstractPlayer.java
+++ src/edu/westga/cs6910/elementalclash/model/AbstractPlayer.java
@@ -3,12 +3,27 @@ package edu.westga.cs6910.elementalclash.model;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * AbstractPlayer provides a base implementation for a player in the Elemental Clash game.
+ * It includes common attributes and methods for both human and computer players.
+ * 
+ * @version 06/23/2024
+ * @author Savitha Venkatesh
+ */
 public abstract class AbstractPlayer implements Play {
     private String name;
     private int lifePoints;
     private Deck deck;
     private List<Card> hand;
 
+    /**
+     * Constructs a new AbstractPlayer with the specified name and deck.
+     * 
+     * @param name the name of the player
+     * @param deck the deck of cards for the player
+     * @precondition name != null && deck != null
+     * @postcondition getName() == name && getLifePoints() == 20 && getHand().size() == 0
+     */
     public AbstractPlayer(String name, Deck deck) {
         this.name = name;
         this.lifePoints = 20;
@@ -16,16 +31,31 @@ public abstract class AbstractPlayer implements Play {
         this.hand = new ArrayList<>();
     }
 
+    /**
+     * Gets the name of the player.
+     * 
+     * @return the name of the player
+     */
     @Override
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Gets the life points of the player.
+     * 
+     * @return the life points of the player
+     */
     @Override
     public int getLifePoints() {
         return this.lifePoints;
     }
 
+    /**
+     * Draws a card from the player's deck and adds it to the player's hand.
+     * 
+     * @return the drawn card
+     */
     @Override
     public Card drawCard() {
         Card card = this.deck.drawCard();
@@ -33,11 +63,23 @@ public abstract class AbstractPlayer implements Play {
         return card;
     }
 
+    /**
+     * Reduces the player's life points by the specified amount.
+     * 
+     * @param points the amount of points to reduce
+     * @precondition points > 0
+     * @postcondition getLifePoints() == old life points - points
+     */
     @Override
     public void reduceLifePoints(int points) {
         this.lifePoints -= points;
     }
 
+    /**
+     * Gets the player's hand.
+     * 
+     * @return the hand of the player
+     */
     @Override
     public List<Card> getHand() {
         return this.hand;
