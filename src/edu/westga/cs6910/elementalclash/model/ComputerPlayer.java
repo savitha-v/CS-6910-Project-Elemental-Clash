2baf0e94600e7ecc137a51cbc4be1a7275b6b62e
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-22 14:20:40 -0500

    created computerplayer

---
+++ src/edu/westga/cs6910/elementalclash/model/ComputerPlayer.java
@@ -0,0 +1,7 @@
+package edu.westga.cs6910.elementalclash.model;
+
+public class ComputerPlayer extends AbstractPlayer {
+    public ComputerPlayer(String name, Deck deck) {
+        super(name, deck);
+    }
+}
