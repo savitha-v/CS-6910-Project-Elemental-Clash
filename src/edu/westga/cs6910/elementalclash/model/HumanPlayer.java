b7cfc5cb47356023da7e492bf56ab9213d28878d
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-22 14:14:48 -0500

    created humanplayer.java

---
+++ src/edu/westga/cs6910/elementalclash/model/HumanPlayer.java
@@ -0,0 +1,7 @@
+package edu.westga.cs6910.elementalclash.model;
+
+public class HumanPlayer extends AbstractPlayer {
+    public HumanPlayer(String name, Deck deck) {
+        super(name, deck);
+    }
+}
