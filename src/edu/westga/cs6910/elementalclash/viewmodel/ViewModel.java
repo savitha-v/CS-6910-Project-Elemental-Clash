0f7d7b1b2c07a2ff41ab684f129d1b1e12844b40
Savitha Venkatesh <svenkat1@csvlab-vm14.uwg.westga.edu>
2024-06-30 12:50:00 -0500

    reverting back to previous viewmodel class

--- src/edu/westga/cs6910/elementalclash/viewmodel/ViewModel.java
+++ src/edu/westga/cs6910/elementalclash/viewmodel/ViewModel.java
@@ -1,7 +1,22 @@
 package edu.westga.cs6910.elementalclash.viewmodel;
 
-import edu.westga.cs6910.elementalclash.model.*;
-import javafx.beans.property.*;
+import edu.westga.cs6910.elementalclash.model.Card;
+import edu.westga.cs6910.elementalclash.model.ComputerPlayer;
+import edu.westga.cs6910.elementalclash.model.Deck;
+import edu.westga.cs6910.elementalclash.model.Game;
+import edu.westga.cs6910.elementalclash.model.HumanPlayer;
+import edu.westga.cs6910.elementalclash.model.Play;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
 
 /**
  * ViewModel provides the bridge between the view and the model for the
@@ -12,81 +27,152 @@ import javafx.beans.property.*;
  * @author Savitha Venkatesh
  */
 public class ViewModel {
-	private Game game;
-	private StringProperty roundResult;
-	private IntegerProperty humanWins;
-	private IntegerProperty computerWins;
+    private Game game;
 
-	/**
-	 * Constructs a new ViewModel and initializes the game.
-	 * 
-	 * @precondition none
-	 * @postcondition game is initialized with a new deck and players
-	 */
-	public ViewModel() {
-		Deck deck = new Deck();
-		this.game = new Game(new HumanPlayer("Human", deck), new ComputerPlayer("Computer", deck), deck);
-		this.roundResult = new SimpleStringProperty();
-		this.humanWins = new SimpleIntegerProperty();
-		this.computerWins = new SimpleIntegerProperty();
-	}
+    private StringProperty roundResult;
+    private IntegerProperty humanWins;
+    private IntegerProperty computerWins;
 
-	/**
-	 * Starts the game by initializing the players and drawing their initial hands.
-	 * 
-	 * @precondition none
-	 * @postcondition players have initial hands drawn and game is ready to play
-	 */
-	public void startGame() {
-		this.game.start();
-		this.roundResult.set("");
-		this.humanWins.set(0);
-		this.computerWins.set(0);
-	}
+    /**
+     * Constructs a new ViewModel and initializes the game.
+     * 
+     * @precondition none
+     * @postcondition game is initialized with a new deck and players
+     */
+    public ViewModel() {
+        Deck deck = new Deck();
+        this.game = new Game(new HumanPlayer("Human", deck), new ComputerPlayer("Computer", deck), deck);
+        this.roundResult = new SimpleStringProperty();
+        this.humanWins = new SimpleIntegerProperty();
+        this.computerWins = new SimpleIntegerProperty();
+    }
 
-	/**
-	 * Plays a round of the game.
-	 * 
-	 * @precondition none
-	 * @postcondition the round result is determined and properties are updated
-	 */
-	public void playRound() {
-		try {
-			this.game.playRound();
-			this.roundResult.set(this.game.getLastRoundResult());
-			this.humanWins.set(((AbstractPlayer) this.game.getHumanPlayer()).getWins());
-			this.computerWins.set(((AbstractPlayer) this.game.getComputerPlayer()).getWins());
-		} catch (IllegalStateException e) {
-			this.roundResult.set("Deck is empty, refilling...");
-			this.game.getDeck().refillDeck();
-			this.playRound();
-		}
-	}
+    /**
+     * Starts the game by initializing the players and drawing their initial hands.
+     * 
+     * @precondition none
+     * @postcondition players have initial hands drawn and game is ready to play
+     */
+    public void startGame() {
+        this.game.start();
+        this.updateWins();
+        this.roundResult.set("");
+    }
 
-	/**
-	 * Gets the round result property.
-	 * 
-	 * @return the round result property
-	 */
-	public StringProperty roundResultProperty() {
-		return this.roundResult;
-	}
+    /**
+     * Plays a round of the game where both players draw a card and a winner is
+     * determined.
+     * 
+     * @precondition none
+     * @postcondition the round result is determined and life points are adjusted
+     *                accordingly
+     */
+    public void playRound() {
+        this.game.playRound();
+        this.roundResult.set(this.game.getLastRoundResult());
+        this.updateWins();
+    }
 
-	/**
-	 * Gets the human wins property.
-	 * 
-	 * @return the human wins property
-	 */
-	public IntegerProperty humanWinsProperty() {
-		return this.humanWins;
-	}
+    /**
+     * Checks if the game is over, which occurs if either player's life points reach
+     * zero.
+     * 
+     * @return true if the game is over, false otherwise
+     */
+    public boolean isGameOver() {
+        return this.game.isGameOver();
+    }
 
-	/**
-	 * Gets the computer wins property.
-	 * 
-	 * @return the computer wins property
-	 */
-	public IntegerProperty computerWinsProperty() {
-		return this.computerWins;
-	}
+    /**
+     * Gets the winner of the game.
+     * 
+     * @return the name of the player who won the game, or "No winner yet" if there
+     *         is no winner
+     */
+    public String getWinner() {
+        Play winner = this.game.getWinner();
+        if (winner != null) {
+            return winner.getName();
+        } else {
+            return "No winner yet";
+        }
+    }
+
+    /**
+     * Gets the human player's hand.
+     * 
+     * @return the list of cards in the human player's hand
+     */
+    public List<Card> getHumanHand() {
+        return this.game.getHumanPlayer().getHand();
+    }
+
+    /**
+     * Gets the computer player's hand.
+     * 
+     * @return the list of cards in the computer player's hand
+     */
+    public List<Card> getComputerHand() {
+        return this.game.getComputerPlayer().getHand();
+    }
+
+    /**
+     * Gets the result of the last round played.
+     * 
+     * @return the result of the last round
+     */
+    public String getLastRoundResult() {
+        return this.game.getLastRoundResult();
+    }
+
+    /**
+     * Restarts the current round without counting it in the game stats.
+     */
+    public void restartRound() {
+        this.game.start();
+    }
+
+    /**
+     * Saves the game state to a file.
+     */
+    public void saveGame() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("game_state.dat"))) {
+            out.writeObject(this.game);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    /**
+     * Loads the game state from a file.
+     */
+    public void loadGame() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream("game_state.dat"))) {
+            this.game = (Game) in.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        this.updateWins();
+        this.roundResult.set("");
+    }
+
+    /**
+     * Updates the win counts for each player.
+     */
+    private void updateWins() {
+        this.humanWins.set(((AbstractPlayer) this.game.getHumanPlayer()).getWins());
+        this.computerWins.set(((AbstractPlayer) this.game.getComputerPlayer()).getWins());
+    }
+
+    public StringProperty roundResultProperty() {
+        return this.roundResult;
+    }
+
+    public IntegerProperty humanWinsProperty() {
+        return this.humanWins;
+    }
+
+    public IntegerProperty computerWinsProperty() {
+        return this.computerWins;
+    }
 }
